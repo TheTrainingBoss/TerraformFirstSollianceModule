71dcfd020eb60c1471d7bdaba78475456ab3de8b
Lino Tadros <thetrainingboss@gmail.com>
2023-04-01 20:57:40 -0400

    My First Commit

---
+++ Locals.tf
@@ -0,0 +1,3 @@
+locals {
+    name = "Lino Tadros"
+}
---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = var.azregion
+}
+
+resource "azurerm_storage_account" "mystorage" {
+  name                     = "linostorage123456789"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+    owner = "Mr.${local.name}"
+  }
+}
+
+
